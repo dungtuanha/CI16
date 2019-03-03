4ba4a1944626c370e1576a1b13dd28f7cd208327
dungtuanha <dungha619@gmail.com>
2019-03-03 15:57:57 +0700

    update

--- src/gamee/GamePanel.java
+++ src/gamee/GamePanel.java
@@ -5,59 +5,27 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class GamePanel extends JPanel {
-    BufferedImage playerImage;
-    Vector2D playerPosition;
-    BufferedImage backgroundImage;
-    Vector2D backgroundPositon;
-    BufferedImage bulletImage;
-    ArrayList<Vector2D> bulletPositions;
-    BufferedImage enemiesImage;
-    Vector2D enemyPosition;
+    Player player;
+    Background background;
+    Enemy enemy;
+
 
     public GamePanel() {
-        playerImage = SpriteUtils.loadImage("assets/images/players/straight/0.png");
-        backgroundImage = SpriteUtils.loadImage("assets/images/background/0.png");
-        playerPosition = new Vector2D(100, 100);
-        backgroundPositon = new Vector2D(0, 600 - 3109);
-        bulletImage = SpriteUtils.loadImage("assets/images/player-bullets/a/1.png");
-        bulletPositions = new ArrayList<>();
-        enemiesImage = SpriteUtils.loadImage("assets/images/enemies/level0/black/0.png");
-        enemyPosition = new Vector2D(0, 100);
+        player = new Player();
+        background = new Background();
+        enemy = new Enemy();
     }
 
     @Override
     public void paint(Graphics g) {
-        g.drawImage(
-                backgroundImage,
-                (int) backgroundPositon.x,
-                (int) backgroundPositon.y,
-                null
-        );
-        g.drawImage(
-                playerImage,
-                (int) playerPosition.x,
-                (int) playerPosition.y,
-                null
-        );
+        background.render(g);
 
-        for (int i = 0; i < bulletPositions.size() ; i++) {
-            Vector2D bullPoosition = bulletPositions.get(i);
-            g.drawImage(
-                    bulletImage,
-                    (int) bullPoosition.x,
-                    (int) bullPoosition.y,
-                    null
-            );
-        }
+        player.render(g);
 
-        g.drawImage(
-                enemiesImage,
-                (int) enemyPosition.x,
-                (int) enemyPosition.y,
-                null
-        );
+        enemy.render(g);
     }
 
     public void gameLoop(){
@@ -78,95 +46,9 @@ public class GamePanel extends JPanel {
     }
 
     private void runAll() {
-        backgroundMove();
-        playerMove();
-        playerLimit();
-        playerFire();
-        bulletsRun();
-        enemyMove();
+        background.run();
+        player.run();
+        enemy.run();
     }
 
-
-    boolean playerGoLeft = true;
-    boolean playerGoRight = true;
-    private void enemyMove() {
-        if(enemyPosition.x == backgroundImage.getWidth()) {
-            playerGoRight = false;
-            playerGoLeft = true;
-        } else if(enemyPosition.x == 0){
-            playerGoLeft = false;
-            playerGoRight = true;
-        }
-
-        if (playerGoRight == true) {
-            enemyPosition.add(1, 0);
-        } else{
-            enemyPosition.add(-1,0);
-        }
-    }
-
-    int frameCount;
-    private void playerFire() {
-        frameCount++;
-        if (GameWindow.isFirePress && frameCount > 20) {
-            Vector2D bulletPosition = playerPosition.clone();
-            bulletPositions.add(bulletPosition);
-            frameCount = 0;
-        }
-    }
-
-    private void bulletsRun() {
-        for (int i = 0; i < bulletPositions.size(); i++) {
-            Vector2D bulletPosition = bulletPositions.get(i);
-            bulletPosition.add(0, -3);
-        }
-    }
-
-    private void playerLimit() {
-        if(playerPosition.x < 0) {
-            playerPosition.set(0, playerPosition.y);
-        }
-
-        if(playerPosition.x > backgroundImage.getWidth() - playerImage.getWidth()){
-            playerPosition.set(
-                    backgroundImage.getWidth() - playerImage.getWidth(),
-                    playerPosition.y
-            );
-        }
-
-        if (playerPosition.y < 0) {
-            playerPosition.set(playerPosition.x, 0);
-        }
-
-        if (playerPosition.y > 600 - playerImage.getHeight()){
-            playerPosition.set(
-                    playerPosition.x,
-                    600 - playerImage.getHeight()
-            );
-        }
-    }
-
-    private void playerMove() {
-        int playerSpeed = 3;
-
-        if(GameWindow.isUpPress){
-            playerPosition.y -= playerSpeed;
-        }
-        if(GameWindow.isDownPress){
-            playerPosition.y += playerSpeed;
-        }
-        if(GameWindow.isLeftPress){
-            playerPosition.x -= playerSpeed;
-        }
-        if(GameWindow.isRightPress){
-            playerPosition.x += playerSpeed;
-        }
-    }
-
-    private void backgroundMove() {
-        backgroundPositon.add(0, 10);
-        if (backgroundPositon.y > 0) {
-            backgroundPositon.set(backgroundPositon.x, 0);
-        }
-    }
 }
